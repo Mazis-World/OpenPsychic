91e2b605df8b78acf820567156b5b39e66b37983
Oumazi Kutlwano <kutlwanomazi@gmail.com>
2023-04-14 00:21:42 -0700

    007 project start

---
+++ app/src/main/java/io/getmadd/openpsychic/HomeFragment.kt
@@ -0,0 +1,44 @@
+package io.getmadd.openpsychic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import io.getmadd.openpsychic.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_HomeFragment_to_LoginFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
