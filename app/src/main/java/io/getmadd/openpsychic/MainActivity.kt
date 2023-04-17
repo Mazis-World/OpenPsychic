6d46cfa5c7741e3ebf941d22e66cd163a825aedf
Oumazi Kutlwano <kutlwanomazi@gmail.com>
2023-04-17 00:09:29 -0700

    crudely implemented admob fullscreen advert on app open

--- app/src/main/java/io/getmadd/openpsychic/MainActivity.kt
+++ app/src/main/java/io/getmadd/openpsychic/MainActivity.kt
@@ -1,15 +1,14 @@
 package io.getmadd.openpsychic
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import io.getmadd.openpsychic.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
