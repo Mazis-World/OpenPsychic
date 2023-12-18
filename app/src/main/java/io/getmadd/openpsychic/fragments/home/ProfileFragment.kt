5850d9731a6854506638e5e29e54a76de85800b9
Oumazi Kutlwano <kutlwanomazi@gmail.com>
2023-12-18 12:28:24 -0700

    q&d

--- app/src/main/java/io/getmadd/openpsychic/fragments/home/ProfileFragment.kt
+++ app/src/main/java/io/getmadd/openpsychic/fragments/home/ProfileFragment.kt
@@ -8,27 +8,27 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.text.Editable
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.firestore
-import com.google.firebase.storage.storage
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import io.getmadd.openpsychic.databinding.FragmentProfileBinding
-
 
 class ProfileFragment : Fragment() {
 
     private lateinit var _binding: FragmentProfileBinding
     private val binding get() = _binding
-    private var db = Firebase.firestore
-    var userId = Firebase.auth.uid
+    private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+    private val storage = FirebaseStorage.getInstance()
+    private var userId = auth.uid
     private var profileImageURI: Uri? = null
     private var backdropImageURI: Uri? = null
     private var selectedView: ImageView? = null
@@ -37,137 +37,124 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var profileImgSrc: String
-        var displayImgSrc: String
 
-        db.collection("users").document("$userId")
-            .get()
-            .addOnSuccessListener { result ->
-                if (result.get("usertype") == "psychic") {
-                    binding.displayNameTV.text = result.get("displayname").toString()
-                } else {
-                    binding.displayNameTV.text = "Base User"
-                }
-                "@${result.get("username").toString()}".also { binding.usersnameTV.text = it }
-                binding.bioEditText.text = Editable.Factory.getInstance().newEditable(result.get("bio").toString())
-                profileImgSrc = result.get("profileImgSrc").toString()
-                displayImgSrc = result.get("displayImgSrcs").toString()
-
-
-
-                //Glide.with(this).load(profileImgSrc).into(binding.backdropImageView);
-                //Glide.with(this).load(displayImgSrc).into(binding.profileImageView);
-
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents.", exception)
-            }
+        setupUI()
 
         binding.backdropImageView.setOnClickListener {
-            val galleryIntent =
-                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            selectedView = binding.backdropImageView
-            getContent.launch(galleryIntent)
+            selectImage(binding.backdropImageView)
         }
         binding.profileImageView.setOnClickListener {
-            val galleryIntent =
-                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            selectedView = binding.profileImageView
-            getContent.launch(galleryIntent)
+            selectImage(binding.profileImageView)
         }
         binding.updateButton.setOnClickListener {
             updateUserDate()
         }
     }
 
+    private fun setupUI() {
+        db.collection("users").document("$userId")
+            .get()
+            .addOnSuccessListener { result ->
+                val userType = result.getString("usertype")
+                binding.displayNameTV.text = if (userType == "psychic") {
+                    result.getString("displayname")
+                } else {
+                    "Base User"
+                }
+                binding.usersnameTV.text = "@${result.getString("username")}"
+                binding.bioEditText.text = Editable.Factory.getInstance()
+                    .newEditable(result.getString("bio"))
+
+                loadImages(result.getString("profileImgSrc"), result.getString("displayImgSrc"))
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
+
+    private fun loadImages(profileImgSrc: String?, displayImgSrc: String?) {
+        if (!profileImgSrc.isNullOrEmpty()) {
+            Glide.with(this).load(profileImgSrc).into(binding.profileImageView)
+        }
+        if (!displayImgSrc.isNullOrEmpty()) {
+            Glide.with(this).load(displayImgSrc).into(binding.backdropImageView)
+        }
+    }
+
+    private fun selectImage(imageView: ImageView) {
+        val galleryIntent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        selectedView = imageView
+        getContent.launch(galleryIntent)
+    }
+
     private val getContent =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-                if (data != null) {
-                    // Handle the selected image
-                    val selectedImage: Uri? = data.data
-                    if (selectedImage != null) {
-                        // Set the selected image to the ImageView
-                        selectedView?.setImageURI(selectedImage)
-                        if (selectedView == binding.backdropImageView) {
-                            backdropImageURI = selectedImage
-                        }
-                        if (selectedView == binding.profileImageView) {
-                            profileImageURI = selectedImage
-                        }
+                result.data?.data?.let { selectedImage ->
+                    selectedView?.setImageURI(selectedImage)
+                    if (selectedView == binding.backdropImageView) {
+                        backdropImageURI = selectedImage
+                    }
+                    if (selectedView == binding.profileImageView) {
+                        profileImageURI = selectedImage
                     }
                 }
             }
         }
 
-
     private fun uploadImageToFirebaseStorage() {
-        // Check if an image is selected
-        if (selectedView != null) {
+        val storageRef = storage.reference
+        val profileImgRef = storageRef.child("images/${auth.uid}/profileImg.jpg")
+        val backdropImgRef = storageRef.child("images/${auth.uid}/backdropImg.jpg")
 
-            // Create a reference to the Firebase Storage
-            val storage = Firebase.storage
-            val storageRef = storage.reference
-            val auth = Firebase.auth
-
-            // Create a reference to the image in storage with a unique name (e.g., user ID + timestamp)
-            val profileImgRef =
-                storageRef.child("images/${auth.uid}/profileImg.jpg")
-
-            val backdropImgRef = storageRef.child("images/${auth.uid}/backdropImg.jpg")
-
-            if (profileImageURI != null) {
-                profileImgRef.putFile(profileImageURI!!)
+        profileImageURI?.let {
+            uploadImage(profileImgRef, it) { profileDownloadUrl ->
+                db.collection("users").document(auth.uid!!)
+                    .update("profileImgSrc", profileDownloadUrl.toString())
                     .addOnSuccessListener {
-                        // Image uploaded successfully
-                        Log.d("UploadImageActivity", "Image uploaded successfully.")
-
-                        db.collection("users").document(auth.uid!!)
-                            .update("profileImageSrc", profileImgRef.downloadUrl)
-
+                        Log.d("UploadImageActivity", "Profile Image URL updated in Firestore.")
                     }
                     .addOnFailureListener { e ->
-                        // Handle the failure
-                        Log.e("UploadImageActivity", "Error uploading image: $e")
+                        Log.e("UploadImageActivity", "Error updating profile image URL in Firestore: $e")
                     }
-            } else {
-                // No image selected
-                Log.e("UploadImageActivity", "No image selected.")
-            }
-
-            if (backdropImageURI != null) {
-                backdropImgRef.putFile(backdropImageURI!!)
-                    .addOnSuccessListener {
-                        // Image uploaded successfully
-                        Log.d("UploadImageActivity", "Image uploaded successfully.")
-                        db.collection("users").document(auth.uid!!)
-                            .update("displayImgeSrc", backdropImgRef.downloadUrl)
-
-                    }
-                    .addOnFailureListener { e ->
-                        // Handle the failure
-                        Log.e("UploadImageActivity", "Error uploading image: $e")
-                    }
-            } else {
-                // No image selected
-                Log.e("UploadImageActivity", "No image selected.")
             }
         }
-        // Upload the images
 
+        backdropImageURI?.let {
+            uploadImage(backdropImgRef, it) { backdropDownloadUrl ->
+                db.collection("users").document(auth.uid!!)
+                    .update("displayImgSrc", backdropDownloadUrl.toString())
+                    .addOnSuccessListener {
+                        Log.d("UploadImageActivity", "Backdrop Image URL updated in Firestore.")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("UploadImageActivity", "Error updating backdrop image URL in Firestore: $e")
+                    }
+            }
+        }
+    }
+
+    private fun uploadImage(imageRef: StorageReference, imageUri: Uri, onSuccess: (String) -> Unit) {
+        imageRef.putFile(imageUri)
+            .addOnSuccessListener { task ->
+                task.storage.downloadUrl.addOnSuccessListener { downloadUrl ->
+                    onSuccess.invoke(downloadUrl.toString())
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e("UploadImageActivity", "Error uploading image: $e")
+            }
     }
 
     private fun updateUserDate() {
-        var updatedBio = binding.bioEditText.text.toString()
-        var auth = Firebase.auth
+        val updatedBio = binding.bioEditText.text.toString()
         if (profileImageURI != null || backdropImageURI != null) {
             uploadImageToFirebaseStorage()
         }
